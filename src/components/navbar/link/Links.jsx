154259aa8934fcff7c380372aaa7ec0daa318e47
payel <payelbhp94@gmail.com>
2024-10-04 17:22:00 +0530

    first

---
+++ src/components/navbar/link/Links.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+
+const Links = () =>{
+    const links = [
+        {
+            title: "Homepage",
+            href : "/"
+        },
+        {
+            title: "About",
+            href : "/about"
+        },
+        {
+            title : "Contact",
+            href : "/contact"
+        },
+        {
+            title : "Blog",
+            href : "/blog"
+        }
+
+]
+
+    return(
+        <div>{links.map(link=>(
+            <Link href={link.href} key={link.title}>{link.title}</Link>
+        ))}</div>
+    )
+}
+
+export default Links
